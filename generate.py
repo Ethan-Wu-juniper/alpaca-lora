6aa666f4a374577e13dc691c54c1708fb9caf8d5
Ethan Wu <304-ethwu0816004@users.noreply.git.cs.nctu.edu.tw>
2023-03-21 10:57:30 +0800

    fix arguments

--- generate.py
+++ generate.py
@@ -89,17 +89,35 @@ class LlamaModel:
 
         return model
 
-    def __call__(self, instruction, input=None, **kwargs):
+    def __call__(
+            self, 
+            instruction,
+            input=None,
+            temperature=0.1,
+            top_p=0.75,
+            top_k=40,
+            num_beams=4,
+            max_new_tokens=128,
+            **kwargs,
+        ):
         device = self.device
         prompt = generate_prompt(instruction, input)
         inputs = self.tokenizer(prompt, return_tensors="pt")
         input_ids = inputs["input_ids"].to(device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
+        )
         with torch.no_grad():
             generation_output = self.model.generate(
                 input_ids=input_ids,
+                generation_config=generation_config,
                 return_dict_in_generate=True,
                 output_scores=True,
-                **kwargs,
+                max_new_tokens=max_new_tokens,
             )
         s = generation_output.sequences[0]
         output = self.tokenizer.decode(s)
