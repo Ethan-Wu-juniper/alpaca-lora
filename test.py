04d02833b3641334289ffc89f94709531ff6db57
Ethan Wu <304-ethwu0816004@users.noreply.git.cs.nctu.edu.tw>
2023-04-24 16:03:01 +0800

    add pbar, time

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from generate import initialized
 import json
+from tqdm.auto import tqdm
+import time
 
 with open("squad.jsonl", "r") as fp:
     data = [json.loads(line) for line in fp.readlines()]
-    results = [initialized(d) for d in data]
+    start_t = time.time()
+    results = [initialized(d) for d in tqdm(data)]
+    print(f"takes {time.time() - start_t} seconds")
